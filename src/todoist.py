2e2f56de03f01c3201ed0f24025bc9fb2e7c6661
Dylan Shade <dpshadey22@gmail.com>
2022-08-21 21:01:38 -0400

    back to normal, time still iffy. doesn't add LIVE assignments

--- src/todoist.py
+++ src/todoist.py
@@ -1,19 +1,25 @@
 import requests, json
 from todoist_api_python.api import TodoistAPI
 import random
-from datetime import datetime
+from datetime import datetime, timezone
+import pytz
+
 
 class Todoist:
     def __init__(self, api):
         self.api = api
 
     def addNewTasks(self, taskName, taskClass, currTasks, dueDate=None, url=None):
+        # Conditional Skips
+        if "LIVE" in taskName:
+            return
+
         labelDict = self.api.get_labels()
         taskDict = self.api.get_tasks()
-      
+
         labelDict = {label.name: label.id for label in labelDict}
         taskIDS = {task.content: task.id for task in taskDict}
-      
+
         # Creates label if it doesn't exist
         if taskClass not in labelDict.keys():
             label = self.api.add_label(
@@ -22,15 +28,26 @@ class Todoist:
             labelDict[taskClass] = label.id
 
         currTaskID = taskIDS.get(f"{taskClass}: {taskName}")
-        if currTaskID:  
-          oldDue = self.api.get_task(task_id=currTaskID).due.datetime
-      
-        if f"{taskClass}: {taskName}" in taskIDS.keys() and oldDue != dueDate:
-            task = self.api.update_task(task_id=currTaskID, due_string=dueDate)
-            print(f"Successfully updated: {taskClass}: {taskName}")
-            return
-        elif f"{taskClass}: {taskName}" in taskIDS.keys() and oldDue == dueDate:
-            return
+        if currTaskID:
+            oldDue = (
+                self.api.get_task(task_id=currTaskID).due.datetime
+                if self.api.get_task(task_id=currTaskID).due is not None
+                else "2001-01-01T00:00:00"
+            )
+
+            if "Z" not in oldDue:
+                oldDue = (
+                    datetime.strptime(oldDue, "%Y-%m-%dT%H:%M:%S")
+                    .astimezone(timezone.utc)
+                    .strftime("%Y-%m-%dT%H:%M:%SZ")
+                )
+
+            if f"{taskClass}: {taskName}" in taskIDS.keys() and oldDue != dueDate:
+                task = self.api.update_task(task_id=currTaskID, due_string=dueDate)
+                print(f"Successfully updated: {taskClass}: {taskName}")
+                return
+            elif f"{taskClass}: {taskName}" in taskIDS.keys() and oldDue == dueDate:
+                return
 
         try:
             task = self.api.add_task(
